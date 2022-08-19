ba9478f6330fd0fa03f08c79892ec123291710da
csiszaralex <csiszaralex@gmail.com>
2022-08-20 00:59:53 +0200

    :wrench: Set up axios baseUrl

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import './registerServiceWorker';
 import router from '@/router';
 import store from '@/store';
 import 'bootstrap';
+import '@/config/axios';
 import BaseButton from './components/UI/BaseButton.vue';
 import BaseInput from './components/UI/BaseInput.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 
 //TODO ha van login átirányít
 
@@ -16,5 +18,6 @@ app.use(router);
 
 app.component('BaseButton', BaseButton);
 app.component('BaseInput', BaseInput);
+app.component('BaseDialog', BaseDialog);
 
 app.mount('#app');
