4cdf31905bcaa945262a627fbdd9492c21311feb
csiszaralex <csiszaralex@gmail.com>
2022-08-19 20:00:03 +0200

    :bug: Adds more details for BaseButton

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -12,18 +12,42 @@ import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-const props = defineProps(['type', 'to', 'outline', 'disabled', 'submit', 'size']);
-
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'primary',
+  },
+  to: {
+    type: String,
+    default: '',
+  },
+  outline: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  submit: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: '',
+  },
+});
 const btnType = computed(() => {
   const classes = [];
-  const type = props.outline === '' ? 'btn-outline-' : 'btn-';
-  classes.push(props.type !== '' ? type + props.type : type + 'info');
+  const type = props.outline ? 'btn-outline-' : 'btn-';
+  classes.push(type + props.type);
   if (props.size === 'lg') classes.push('btn-lg');
   if (props.size === 'sm') classes.push('btn-sm');
   return classes;
 });
 const btnSend = computed(() => {
-  if (!props.to && props.submit === '') {
+  if (!props.to && props.submit) {
     return 'submit';
   }
   return 'button';
