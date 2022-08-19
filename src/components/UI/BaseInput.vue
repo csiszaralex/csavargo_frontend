7b3f558a04c43264b81d001a9f2adac370349c44
csiszaralex <csiszaralex@gmail.com>
2022-08-19 23:16:46 +0200

    :art: Adds BaseInput and use in Login form

---
+++ src/components/UI/BaseInput.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="inputBox">
+      <input type="text" required="required" v-model="value" :autofocus="autofocus" />
+      <span>Kód</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps({
+  modelValue: { type: String, required: true },
+  autofocus: { type: Boolean, default: false },
+});
+const emits = defineEmits(['update:modelValue']);
+
+const value = ref(props.modelValue ? props.modelValue : '');
+watch(
+  () => props.modelValue,
+  (val, oldVal) => {
+    if (val !== oldVal) {
+      value.value = val;
+    }
+  },
+);
+watch(value, val => {
+  emits('update:modelValue', val);
+});
+</script>
+
+<style lang="scss" scoped>
+$szin1: darken($primary, 10%);
+$okay: $primary;
+$size: 3rem;
+
+.inputBox {
+  position: relative;
+  width: 85vw;
+  input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid $szin1;
+    background: $bg;
+    border-radius: 5px;
+    outline: none;
+    color: $dark;
+    font-size: $size;
+    transition: 0.5s;
+    &:valid ~ span,
+    &:focus ~ span {
+      color: $bg;
+      transform: translateX(0.3 * $size) translateY(-0.48 * $size);
+      font-size: 0.65 * $size;
+      padding: 0 10px;
+      background: $okay;
+      letter-spacing: 0.2em;
+      border-radius: 2px;
+    }
+    &:valid,
+    &:focus {
+      border-color: $okay;
+    }
+  }
+  span {
+    position: absolute;
+    left: 0;
+    padding: 10px;
+    pointer-events: none;
+    font-size: $size;
+    color: $szin1;
+    text-transform: uppercase;
+    transition: 0.5s;
+  }
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="d-flex flex-column align-items-center justify-content-around">
     <div class="form">
-      
+      <base-input v-model="inp" autofocus>Belépési kód</base-input>
       <base-button type="danger" size="lg" outline>Belépés</base-button>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue';
+
+const inp = ref('');
+watch(inp, (val, oldVal) => {
+  if (val !== oldVal.toUpperCase()) {
+    inp.value = val.toUpperCase();
+  }
+});
+</script>
 
 <style lang="scss" scoped>
 .form {
+  text-align: center;
   button {
     font-size: 3.5rem;
     font-weight: 700;
     border-width: 0.3rem;
+    margin: 25px;
   }
 }
 </style>
