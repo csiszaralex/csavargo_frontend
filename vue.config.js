cb578a986b58e6d4a1b844de4dadb5850f7200e2
csiszaralex <csiszaralex@gmail.com>
2022-08-25 01:51:10 +0200

    :bento: Update manifest, icons and PWA support

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  pwa: {
+    name: 'Csavargó',
+    themeColor: '#A45EE5',
+    msTileColor: '#E59E0F',
+    workboxPluginMode: 'GenerateSW',
+    iconPaths: {
+      faviconSVG: 'img/icons/favicon.svg',
+      // favicon192: 'img/icons/csavargo192.png',
+      // favicon96: 'img/icons/csavargo96.png',
+      // favicon72: 'img/icons/csavargo72.png',
+      favicon32: 'img/icons/favicon32x32.png',
+      favicon16: 'img/icons/favicon16x16.png',
+      appleTouchIcon: 'img/icons/ios.png',
+      maskIcon: 'img/icons/favicon.svg',
+      msTileImage: 'img/icons/msTile.png',
+    },
+  },
   css: {
     loaderOptions: {
       sass: {
