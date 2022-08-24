a70d0c4b91d6210f8bd7734a3fab547416d24120
csiszaralex <csiszaralex@gmail.com>
2022-08-24 23:27:16 +0200

    :rocket: Update production backend link

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://10.0.0.10:3000/' : 'https://csalex.dev:3000/';
+  process.env.NODE_ENV === 'development' ? 'http://10.0.0.10:3000/' : 'http://gt.jedlik.eu:3000/';
