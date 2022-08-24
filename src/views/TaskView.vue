da370f3bbd49d54ece180d5c6ccfd6f6f489db69
csiszaralex <csiszaralex@gmail.com>
2022-08-24 23:32:15 +0200

    :sparkles: Adds option for file upload

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -2,11 +2,18 @@
   <div class="test">
     <div class="question">{{ question }}</div>
     <form @submit.prevent="kuld">
-      <base-input v-model="answer" large>Válasz</base-input>
+      <base-input v-if="type === 'text'" v-model="answer" large>Válasz</base-input>
+      <div v-else class="file">
+        <base-file @hasFile="hasFile" :file="file.file"></base-file>
+      </div>
       <base-button outline class="kuld" @click="kuld">&nbsp;&nbsp;Küldés&nbsp;&nbsp;</base-button>
     </form>
     <div class="attempts">
-      <div v-for="i in 3" :key="i" class="attempt" :class="i <= meg ? 'has' : ''"></div>
+      <div
+        v-for="i in type === 'text' ? 3 : 1"
+        :key="i"
+        class="attempt"
+        :class="i <= meg ? 'has' : ''"></div>
     </div>
     <base-button outline type="danger" class="felad" @click="felad">Feladom</base-button>
     <base-dialog :show="!!error.body" @close="tryClose" :title="error.header" :type="error.type">
@@ -27,8 +34,10 @@ const router = useRouter();
 const taskId = computed(() => store.getters.getTask);
 
 const question = ref('');
-const meg = ref(1);
+const meg = ref(3);
 const answer = ref('');
+const file = reactive({ file: '' });
+const type = ref('text');
 const error = reactive({ body: '', type: '', header: '' });
 
 function tryClose() {
@@ -36,37 +45,75 @@ function tryClose() {
   if (meg.value === 0) router.replace('/');
 }
 function kuld() {
-  if (answer.value.length === 0) return;
-  axios
-    .post(`feladat/try/${taskId.value}`, { answer: answer.value })
-    .then(({ data }) => {
-      if ('type' in data) {
+  if (type.value === 'text') {
+    if (answer.value.length === 0) return;
+    axios
+      .post(`feladat/try/${taskId.value}`, { answer: answer.value })
+      .then(({ data }) => {
+        if ('type' in data) {
+          error.type = 'success';
+          error.header = 'Siker';
+          error.body = `Sikerült megtalálni a megoldást, és még ${meg.value} próbbálkozás maradt is!`;
+          meg.value = 0;
+        } else if ('voltmar' in data) {
+          error.type = 'primary';
+          error.header = 'Ez volt már!';
+          error.body = `Ezt már próbáltátok, és még mindig nem jó, de nyugi, legalább a próbálkozások száma nem változott, még mindig ${meg.value}!`;
+        } else {
+          meg.value = data.meg;
+          if (data.meg === 0)
+            throw { response: { data: { message: 'Sajnos ez volt az  utolsó próbálkozás!' } } };
+          error.type = 'warning';
+          error.header = 'Ez nem talált!';
+          error.body = `Ez most nem jött össze, de ne csüggedj, még van ${data.meg} próbálkozásotok!`;
+        }
+      })
+      .catch(err => maxTry(err));
+  } else {
+    const maxSize = 1024 * 1024 * 15;
+    if (!file.file) return;
+    if (file.file.size > maxSize) {
+      fileBig();
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', file.file);
+    axios
+      .post(`feladat/upload/${taskId.value}`, formData)
+      .then(() => {
+        error.header = 'Sikeres feltöltés';
         error.type = 'success';
-        error.header = 'Siker';
-        error.body = `Sikerült megtalálni a megoldást, és még ${meg.value} próbbálkozás maradt is!`;
-        meg.value = 0;
-      } else if ('voltmar' in data) {
-        error.type = 'primary';
-        error.header = 'Ez volt már!';
-        error.body = `Ezt már próbáltátok, és még mindig nem jó, de nyugi, legalább a próbálkozások száma nem változott, még mindig ${meg.value}!`;
-      } else {
-        meg.value = data.meg;
-        if (data.meg === 0)
-          throw { response: { data: { message: 'Sajnos ez volt az  utolsó próbálkozás!' } } };
-        error.type = 'warning';
-        error.header = 'Ez nem talált!';
-        error.body = `Ez most nem jött össze, de ne csüggedj, még van ${data.meg} próbálkozásotok!`;
-      }
-    })
-    .catch(err => {
-      error.type = 'danger';
-      error.header = 'Elfogyott a próbálkozás!';
-      error.body = err.response.data.message;
-    });
+        error.body = 'A feltöltés sikeres, rövidesen elbírálásra is kerül a feladat!';
+        file.file = '';
+        meg.value--;
+      })
+      .catch(err => {
+        if (err.response.status === 413) {
+          fileBig();
+          return;
+        }
+        maxTry(err);
+      });
+  }
+}
+function fileBig() {
+  error.header = 'Hibás fájlméret!';
+  error.type = 'danger';
+  error.body = 'A feltöltött fájl mérete túl nagy, maximum 15MB lehet.';
+  file.file = '';
+}
+function maxTry(err) {
+  error.type = 'danger';
+  error.header = 'Elfogyott a próbálkozás!';
+  error.body = err.response.data.message;
 }
 function felad() {
   store.dispatch('removeTasks');
   router.push('/');
+}
+
+function hasFile(fi) {
+  file.file = fi.file;
 }
 
 onBeforeMount(() => {
@@ -78,6 +125,7 @@ onBeforeMount(() => {
     .get(`/feladat/get/${taskId.value}`)
     .then(({ data }) => {
       question.value = data.feladat;
+      type.value = data.type;
       meg.value = data.meg;
     })
     .catch(err => {
